8ee80fe48b06a023ea34cbd6751eef8c11d439c2
ubantu <xman@master.demo.lab>
2023-07-25 21:03:03 +0530

    cal file

---
+++ cal.py
@@ -0,0 +1,11 @@
+from add import *
+from sub import *
+
+num1=int(input("enter the first no.:"))
+num2=int(input("enter the second no.:"))
+
+a1=add(num1,num2)
+a2=sub(num1,num2)
+print("the addition is :",a1)
+print("the substraction is :",a2)
+
